d1c681cb9b92c75b40297a61d86c32b30095023d
ChivasQ <a6plus18@gmail.com>
2026-08-08 15:00:53 +0300

    test details

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,4 +17,9 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
 }
